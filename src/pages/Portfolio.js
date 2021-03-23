71752cb18ee8909fed8b7e66d15586c7f3c1787f
angelia collins <angiecollins09@gmail.com>
2021-03-22 19:43:09 -0500

    added portfolio page stuff

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Block from "../components/Block/index";
 import Header from "../components/Header/index";
+import corona from "../assets/corona.png";
+import creeper from "../assets/creeper.png";
+import weather from "../assets/weather.png";
 
 function Portfolio() {
   return (
@@ -9,6 +12,24 @@ function Portfolio() {
       <Block>
         <Header>Portfolio</Header>
         <div className="dropdown-divider"></div>
+        <h4>Corona Survival</h4>
+        <a href="https://mpapamichalis.github.io/Corona-Survival/" target="_blank"><img src={corona} className="img-fluid" alt="placeholder image"/></a>
+        <br />
+        <a href="https://mpapamichalis.github.io/Corona-Survival/" target="_blank">Deployed Project</a>
+        <br />
+        <a href="https://github.com/angelia-collins/Corona-Survival" target="_blank">Github Repo</a>
+        <h4>Creeper Crew</h4>
+        <a href="https://powerful-shelf-96564.herokuapp.com/" target="_blank"><img src={creeper} className="img-fluid" alt="placeholder image"/></a>
+        <br />
+        <a href="https://powerful-shelf-96564.herokuapp.com/" target="_blank">Deployed Project</a>
+        <br />
+        <a href="https://github.com/angelia-collins/creeper-crew" target="_blank">Github Repo</a>
+        <h4>Weather Dashboard</h4>
+        <a href="https://angelia-collins.github.io/weather-dashboard/" target="_blank"><img src={weather} className="img-fluid" alt="placeholder image"/></a>
+        <br />
+        <a href="https://angelia-collins.github.io/weather-dashboard/" target="_blank">Deployed Project</a>
+        <br />
+        <a href="https://github.com/angelia-collins/weather-dashboard" target="_blank">Github Repo</a>
 
 
       </Block>
